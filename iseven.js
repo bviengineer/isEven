f875855e1ea10baf7667bcf770b77195a9df988d
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-11-25 12:05:20 -0500

    changed parstInt method to parseFloat to account for decimal numbers

--- iseven.js
+++ iseven.js
@@ -1,7 +1,7 @@
 //Write a function isEven(), which takes a single numeric argument and returns true if the number is even, and false otherwise
 
 function isEven(input){
-    var num = parseInt(input);
+    var num = parseFloat(input);
     if(num % 2 === 0){
         return true;
     } else {
