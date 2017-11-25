d073db28cd7732ec31ae554d955702d4f77f5675
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-11-25 10:44:50 -0500

    added p tag to index page, updated note in js file, created function in js file

--- iseven.js
+++ iseven.js
@@ -1,2 +1,11 @@
-//Write a function isEven() which takes a single numeric argument and returns true if the number is even, and false otherwise
+//Write a function isEven(), which takes a single numeric argument and returns true if the number is even, and false otherwise
+
+function isEven(input){
+    var num = parseInt(input);
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
 
